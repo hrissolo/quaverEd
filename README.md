cce35358393262d986716941b6f67ce9ead8c198
hrissolo <hrissolo@gmail.com>
2021-04-06 13:48:41 -0500

    both counters working

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # quaverEd
+Hailey Rissolo April 2021 QuaverEd coding challenge :-) 
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
 export const speech = "We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defense, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America."
 export const speechArray = speech.split(" ");
 
-
 let counterT = 0;
 for (let i=0; i < speechArray.length; i++) {
     if (speechArray[i].startsWith("t")) {
@@ -22,9 +21,16 @@ for (let i=0; i < speechArray.length; i++) {
     }
 }
 
+let counterBoth = 0;
+for (let i=0; i < speechArray.length; i++) {
+    if (speechArray[i].endsWith("e") && (speechArray[i].startsWith("t"))) { 
+        counterBoth++
+    } 
+}
 
 const wordCountTest = `The speech has ${speechArray.length} words. <br>
-There are ${counterT} words starting with the letter T. <br>
-There are ${counterE} words ending with the letter E.`
+    There are <b>${counterT}</b> words starting with the letter T. <br>
+    There are <b>${counterE}</b> words ending with the letter E. <br>
+    There are <b>${counterBoth}</b> words both starting with T & ending with E.`
 
 document.getElementById("wordCounter").innerHTML = wordCountTest;
